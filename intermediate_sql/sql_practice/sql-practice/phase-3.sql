be193ccc1d3781ffb55940693bbc50e86c6591eb
nathanieldcooke <nathanield.cooke@gmail.com>
2021-11-18 10:57:46 -0800

    complete phase 3

--- intermediate_sql/sql_practice/sql-practice/phase-3.sql
+++ intermediate_sql/sql_practice/sql-practice/phase-3.sql
@@ -1,5 +1,18 @@
 -- Find Hermione's cats
 -- Your code here
 
+SELECT cats.name 
+FROM owners
+JOIN cat_owners ON (owners.id == cat_owners.owner_id)
+JOIN cats ON (cats.id == cat_owners.cat_id)
+WHERE (owners.first_name == 'Hermione');
+
 -- Find All the Toys for Hermione's cats
 -- Your code here
+
+SELECT toys.name 
+FROM owners
+JOIN cat_owners ON (owners.id == cat_owners.owner_id)
+JOIN cats ON (cats.id == cat_owners.cat_id)
+JOIN toys ON (toys.cat_id == cats.id)
+WHERE (owners.first_name == 'Hermione');
